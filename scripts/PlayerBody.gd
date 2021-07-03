9899c98f6b0daf59c6a8a7a65d66026045329767
cameron.dugan <cameron.dugan@protonmail.com>
2021-07-03 10:40:39 -0400

    fixed movement

--- scripts/PlayerBody.gd
+++ scripts/PlayerBody.gd
@@ -12,38 +12,42 @@ onready var L_controller = get_tree().get_nodes_in_group("left controller")[0]
 onready var headset = get_tree().get_nodes_in_group("head")[0]
 onready var vr_origin = get_tree().get_nodes_in_group("origin")[0]
 
+var igrounded=false
+var impact = false
 func _physics_process(delta):
-	#update variables
-	var rc=Vector2(R_controller.translation.x,R_controller.translation.z)
-	var lc=Vector2(L_controller.translation.x,L_controller.translation.z)
-	var d=rc.distance_to(lc)
-	var h=R_controller.translation.y-L_controller.translation.y
-	var player_height=headset.translation.y
-	var tmp = player_height*0.7
-	var floating=d>tmp
-	
 	var count = get_slide_count()
 	var grounded=count>0
 	if (grounded):
 		velocity*=friction
+	var impact = not igrounded and grounded
 	#movement physics
 	if (ciforce>0):
 		iforce/=ciforce
 		velocity=iforce
 	else:
-		var flying = floating and !grounded
-		if (flying):
-			var push = -headset.get_global_transform().basis[2]
-			push.y=0
-			push = push.normalized()
-			velocity+=push*delta
-			velocity.y=max(-0.3,velocity.y)
-			var turn = h*PI/4*delta
-			velocity=velocity.rotated(Vector3.UP,turn)
-			#pick one not both
-			#transform = transform.rotated(Vector3.UP,turn)
-			transform.basis = transform.basis.rotated(Vector3.UP, turn*1.1)
+		var rc=Vector2(R_controller.translation.x,R_controller.translation.z)
+		var lc=Vector2(L_controller.translation.x,L_controller.translation.z)
+		var d=rc.distance_to(lc)
+		var player_height=headset.translation.y
+		var tmp = player_height*0.8
+		var floating=d>tmp
+		fly(floating and !grounded,delta)
 	velocity.y -= gravity*delta
 	ciforce=0
 	iforce=Vector3.ZERO
-	velocity = move_and_slide(velocity,Vector3.UP)
+	velocity = move_and_slide(velocity)
+	igrounded=grounded
+
+func fly(is_flying,delta):
+	if (is_flying):
+		var push = -headset.get_global_transform().basis[2]
+		var h=R_controller.translation.y-L_controller.translation.y
+		push.y=0
+		push = push.normalized()
+		velocity+=push*delta
+		velocity.y=max(-0.3,velocity.y)
+		var turn = h*PI/4*delta
+		velocity=velocity.rotated(Vector3.UP,turn)
+		#pick one not both
+		#transform = transform.rotated(Vector3.UP,turn)
+		transform.basis = transform.basis.rotated(Vector3.UP, turn*1.1)
--- scripts/controller.gd
+++ scripts/controller.gd
@@ -6,7 +6,7 @@ onready var rc = get_tree().get_nodes_in_group("right controller")[0]
 
 var target_controller = null
 export var cID = 0
-export var speed = 25
+export var speed = 20
 
 func _ready():
 	var mesh
@@ -23,19 +23,24 @@ func _ready():
 var slide
 func _physics_process(_delta):
 	if (target_controller):
+		#rotation
+		self.global_transform.basis = target_controller.global_transform.basis
+		
+		#physics
 		var dir = target_controller.global_transform.origin - self.global_transform.origin
-		if (dir.length()>0.3):
+		#snap if stuck
+		print(dir.length())
+		if (dir.length()>0.35):
 			self.global_transform.origin = target_controller.global_transform.origin
 		dir*=speed
-		#snap if stuck
 		slide = move_and_slide(dir)
 		var count = get_slide_count()
 		var col = count>0
 		#collision
 		if (col):
-			var move = -dir/speed * 15
+			var collis = get_slide_collision(0)
+			var move = collis.normal * dir.length() * 3/5
 			#hand friction
-			move-=slide*0.3
+			move-=slide
 			pbody.iforce += move
 			pbody.ciforce +=1
-		self.rotation = target_controller.rotation
